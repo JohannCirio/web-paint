aad07e04afa8074c84fd5e0bb1516c0e09011f70
Johann <johannocsantos@gmail.com>
2020-06-15 17:28:49 -0300

    Finished the 'hard' part of the project, now I'm gonna make it look better by giving it some CSS.

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 const div = document.createElement("div");
 const bigGrid = document.querySelector("#bigGrid");
 const resetButton = document.querySelector("#reset");
+const changeGrid = document.querySelector("#newSize");
+const colorPicker = document.querySelector("#colorPicker");
+
+const brushButton = document.querySelector("#brushMode");
+const clickButton = document.querySelector("#clickMode");
+const eraserButton = document.querySelector("#eraserMode");
 
 let texto = document.createElement("p");
 
+let brushOn = true;
 
-for (let index = 0; index < 16; index++) {
+function createGrid (lado) {
+
+    for (let index = 0; index < lado; index++) {
     
-    for(let index2 = 0; index2 <16; index2++){
-        texto = document.createElement("div");
-        texto.classList.add("square");
-        //texto.textContent = `${index}${index2}`;
-        bigGrid.appendChild(texto);
-    } 
+        for(let index2 = 0; index2 < lado; index2++){
+            texto = document.createElement("div");
+            texto.classList.add("square");
+            bigGrid.appendChild(texto);
+        } 
+    }
+
 }
 
+function gridSize () {
 
+    let lado = prompt("Digite a quantidade de quadrados de lado:", Number);
+    
+    while(bigGrid.firstChild){
+        bigGrid.removeChild(bigGrid.lastChild);
+    }
 
-function teste (event) {
-
-    //console.log("qualquer coisa");
-    //console.log(event);
-
-    const alvo = event.target.className;
-
-    if(alvo === "square"){
-        //event.target.classList.add("red");
-        event.target.style.backgroundColor = "red";
-    }   
-   
+    bigGrid.style.gridTemplateColumns = `repeat(${lado},1fr)`;
+    bigGrid.style.gridTemplateRows = `repeat(${lado},1fr)`;
+    createGrid(lado);
 }
 
 function resetColor () {
@@ -39,9 +46,69 @@ function resetColor () {
     for(i = 0; i < squarelist.length; i++){
 
         squarelist[i].style.backgroundColor = "white";
+        squarelist[i].style.opacity = "0";
     }
     
 }
 
-bigGrid.addEventListener("mouseover",teste);
+function coloringBrush (event) {
+
+    let alvo = event.target.className;
+
+    if (!brushOn) {
+        return;
+    }
+
+    if(alvo === "square"){
+        event.target.style.backgroundColor = colorPicker.value;
+    }
+   
+}
+
+function proMode (event) {
+
+    let alvo = event.target.className;
+
+    if (!eraserOn) {
+        return;
+    }
+
+    if(alvo === "square"){
+        event.target.style.backgroundColor = colorPicker.value;
+        let opacidade = event.target.style.opacity;
+        opacidade = Number(opacidade) + 0.1;
+        event.target.style.opacity = opacidade;
+    }
+
+    
+}
+
+function setBrush () {
+    brushOn = true;
+    eraserOn = false;
+    clickOn = false;
+}
+
+function setClick () {
+    brushOn = false;
+    eraserOn = false;
+    clickOn = true;
+}
+
+function setEraser() {
+    brushOn = false;
+    eraserOn = true;
+    clickOn = false;
+}
+
+createGrid(16);
+colorPicker.value = "#222222";
+
+
+bigGrid.addEventListener("mouseover", coloringBrush);
 resetButton.addEventListener("click", resetColor);
+changeGrid.addEventListener("click", gridSize);
+brushButton.addEventListener("click", setBrush);
+eraserButton.addEventListener("click", setEraser);
+clickButton.addEventListener("click", setClick);
+
